4ecd09d80ef8a7751bd32f11777bff7bb194f8bd
torahrd <hrd_trnsk@icloud.com>
2025-04-29 09:55:17 +0000

    step2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,4 +26,9 @@ class Post extends Model
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
