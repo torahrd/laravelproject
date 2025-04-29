77cdae4018a6ec8215b8768b2b0805f4ad360505
torahrd <hrd_trnsk@icloud.com>
2025-04-29 13:56:23 +0000

    curriculum08-2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスを使えるようにします。
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,5 @@ Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{pst}', [PostController::class, 'update']);
 
 Route::delete('/posts/{post}', [PostController::class, 'delete']);
+
+Route::get('/categories/{category}', [CategoryController::class, 'index']);
