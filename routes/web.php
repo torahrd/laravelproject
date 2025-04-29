7b3ed8247f7c727347c55accc0f6ddd298886f34
torahrd <hrd_trnsk@icloud.com>
2025-04-29 14:16:30 +0000

    step1

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスを使えるようにします。
-use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,17 +14,18 @@ use App\Http\Controllers\CategoryController;
 |
 */
 
-Route::get('/', [PostController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/posts', [PostController::class, 'store']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
-Route::put('/posts/{pst}', [PostController::class, 'update']);
-
-Route::delete('/posts/{post}', [PostController::class, 'delete']);
-
-Route::get('/categories/{category}', [CategoryController::class, 'index']);
+require __DIR__.'/auth.php';
