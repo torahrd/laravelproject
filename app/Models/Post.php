e1c9eb271320c8664daffbcaa43347452880749a
torahrd <hrd_trnsk@icloud.com>
2025-04-29 10:32:13 +0000

    step4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,15 +17,15 @@ class Post extends Model
     ];
 
     use HasFactory;
-    public function getByLimit(int $limit_count = 10)
+    public function getByLimit(int $limit_count = 5)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
     }
-    public function getPaginateByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 
     public function category()
