83c0a02589e252393e14e32230813e0cfcfadbc8
torahrd <hrd_trnsk@icloud.com>
2025-04-30 10:08:57 +0000

    curriclum08-4

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -40,6 +40,16 @@
       <p>ログインユーザー: {{ Auth::user()->name  }}</p>
     </div>
 
+    <div>
+      @foreach($questions as $question)
+      <div>
+        <a href="https://teratail.com/questions/{{ $question['id'] }}">
+          {{ $question['title'] }}
+        </a>
+      </div>
+      @endforeach
+    </div>
+
     <script>
       function deletePost(id) {
         'use strict'
